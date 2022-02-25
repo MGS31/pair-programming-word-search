36409596b467fd6bc1f04f0c2de3726e8176aaa4
Matt <mgstankov30@gmail.com>
2022-02-24 19:28:15 -0500

    completed word search code

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+
+    let resultMatrix = [];
+    for (let r = 0; r < matrix.length; r++) {
+      for (let c = 0; c < matrix[r].length; c++) {
+        // check if resultMatrix exists, if not, create it. Otherwise proceed and push the number into it.
+        if (!resultMatrix[c]) {
+          resultMatrix.push([]);
+        }
+        resultMatrix[c].push(matrix[r][c]);
+      }
     }
-}
+    return resultMatrix;
+  };
+  
+  const wordSearch = (letters, word) => { 
+    for (i = 1; i <=2; i++) {
+      const horizontalJoin = letters.map(ls => ls.join('')+', '+ls.reverse().join(''))
+      for (l of horizontalJoin) {
+        if (l.includes(word)) return true
+      }
+      letters = transpose(letters);
+    }
+    return false;
+};
 
 module.exports = wordSearch
